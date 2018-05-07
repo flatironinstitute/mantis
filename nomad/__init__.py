816f2bc1f79c6a6b779e54037caf1cf96018dd37
marianotepper <Lulita75>
2018-05-07 11:45:03 -0400

    Change module name

--- nomad/__init__.py
+++ nomad/__init__.py
@@ -1,6 +1,6 @@
 from __future__ import absolute_import, print_function
 from nomad.embedding import nomad_embedding, spectral_embedding
 from nomad.nmf import symnmf_admm
-from nomad.sdp import nomad, sdp_km_burer_monteiro,\
+from nomad.solvers import nomad, sdp_km_burer_monteiro,\
     sdp_km_conditional_gradient
 from nomad.utils import connected_components, dot_matrix, log_scale
