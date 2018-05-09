5524d0ffab4799a6806a4a222b4b612d12a827bf
marianotepper <Lulita75>
2018-05-09 16:44:19 -0400

    Simplified symmetric NMF code

--- nomad/nmf.py
+++ nomad/nmf.py
@@ -12,7 +12,7 @@ def symnmf_admm(A, k, H=None, maxiter=1e3, tol=1e-5, sigma=1):
         raise ValueError('A must be a symmetric matrix!')
 
     if H is None:
-        H = np.sqrt(A.mean() / k) * np.random.randn(n, k)
+        H = np.sqrt(np.abs(A.mean()) / k) * np.random.randn(n, k)
         np.abs(H, H)
 
     Gamma = np.zeros((n, k))
@@ -36,32 +36,28 @@ def symnmf_admm(A, k, H=None, maxiter=1e3, tol=1e-5, sigma=1):
     return W
 
 
-def symnmf_gram_admm(A, k, H=None, maxiter=1e3, tol=1e-5, sigma=1):
+class DecomposedArray:
     """
-    Solves || A.dot(A.T) - W.dot(W.T) ||_F^2 s.t. W >= 0
+    M = X.dot(X.T) + c * np.ones((n, n))
+    where n = len(X)
     """
-    if H is None:
-        n = A.shape[0]
-        v = A.T.dot(np.ones((n,)))
-        H = np.sqrt(v.dot(v) / (k * n ** 2)) * np.random.randn(n, k)
-        np.abs(H, H)
+    def __init__(self, X, c=0):
+        self.X = X
+        self.c = c
 
-    Gamma = np.zeros((n, k))
-    id_k = np.identity(k)
-    step = 1
+    def dot(self, Y):
+        n = len(self.X)
+        ones = np.ones((n, 1))
+        return self.X.dot(self.X.T.dot(Y)) + self.c * ones.dot(ones.T.dot(Y))
 
-    error = []
-    for i in range(int(maxiter)):
-        temp = np.linalg.inv(H.T.dot(H) + sigma * id_k)
-        W = (A.dot(A.T.dot(H)) + sigma * H - Gamma).dot(temp)
-        W = np.maximum(W, 0)
-        temp = np.linalg.inv(W.T.dot(W) + sigma * id_k)
-        H = (A.dot(A.T.dot(W)) + sigma * W + Gamma).dot(temp)
-        H = np.maximum(H, 0)
-        Gamma += step * sigma * (W - H)
+    def mean(self):
+        n = len(self.X)
+        v = self.X.T.dot(np.ones((n,)))
+        mean = (v.dot(v) / (n ** 2)) + self.c
+        print(mean)
+        return mean
 
-        error.append(np.linalg.norm(W - H) / np.linalg.norm(W))
-        if i > 0 and np.abs(error[-1]) < tol:
-            break
-
-    return W
+    @property
+    def shape(self):
+        n = len(self.X)
+        return n, n
