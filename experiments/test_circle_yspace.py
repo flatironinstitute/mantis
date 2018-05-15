dad9e7dc594a05862609cc021e3f2c0b831ae2c6
marianotepper <Lulita75>
2018-05-15 08:03:47 -0400

    Add cosine to summary plot of aligned bumps

--- experiments/test_circle_yspace.py
+++ experiments/test_circle_yspace.py
@@ -94,15 +94,19 @@ def test_one_circle(n_clusters=8, use_copositive=False):
                                       tol=1e-5, constraint_tol=1e-5,
                                       verbose=True)
         name = 'circle_copositive_bm'
+        cos_freq = 0.13
     else:
         Y = sdp_km_burer_monteiro(X, n_clusters, rank=rank, tol=1e-5,
                                   verbose=True)
         name = 'circle_sdpkm_bm'
+        cos_freq = 0.2
 
     Q = Y.dot(Y.T)
 
     idx = np.argsort(np.argmax(Y, axis=0))
     Y = Y[:, idx]
+
+    Y_aligned = align_bumps(Y, Y.shape[1] // 2)
 
     sns.set_style('white')
 
@@ -127,13 +131,13 @@ def test_one_circle(n_clusters=8, use_copositive=False):
 
     plt.savefig('{}{}.pdf'.format(dir_name, name), dpi=300)
 
-    pdf_file_name = '{}{}_plot_{}_on_data_{}{}'
-    for i in range(Y.shape[1]):
-        plt.figure()
-        plot_bumps_on_data(X, [Y[:, i]])
-        plt.savefig(pdf_file_name.format(dir_name, name, 'Y', i, '.png'),
-                    dpi=300)
-        plt.close()
+    # pdf_file_name = '{}{}_plot_{}_on_data_{}{}'
+    # for i in range(Y.shape[1]):
+    #     plt.figure()
+    #     plot_bumps_on_data(X, [Y[:, i]])
+    #     plt.savefig(pdf_file_name.format(dir_name, name, 'Y', i, '.png'),
+    #                 dpi=300)
+    #     plt.close()
 
     pdf_file_name = '{}{}_plot_{}_on_data_{}'
     plt.figure()
@@ -141,8 +145,6 @@ def test_one_circle(n_clusters=8, use_copositive=False):
     plot_bumps_on_data(X, [Y[:, i] for i in bumps_locs], palette='Set1')
     plt.savefig(pdf_file_name.format(dir_name, name, 'Y', 'multiple.png'),
                 dpi=300)
-
-    Y_aligned = align_bumps(Y, Y.shape[1] // 2)
 
     _, ax = plt.subplots(1, 1)
     plot_matrix(Y_aligned, ax=ax)
@@ -164,19 +166,29 @@ def test_one_circle(n_clusters=8, use_copositive=False):
     plt.savefig('{}{}Y_aligned_1d.pdf'.format(dir_name, name), dpi=300)
 
     pos = np.arange(len(Y))
-    median = np.median(Y_aligned, axis=1)
+    # median = np.median(Y_aligned, axis=1)
     mu = np.mean(Y_aligned, axis=1)
     sigma = np.std(Y_aligned, axis=1)
 
+    def cosine(x):
+        peak = np.argmax(mu)
+        cos_loc = np.mean(np.where(mu == mu[peak])[0])
+        cos_bump = mu[peak] * np.cos(cos_freq * (x - cos_loc))
+        cos_bump[mu == 0] = 0
+        cos_bump[cos_bump < 0] = 0
+        return cos_bump
+
     _, ax = plt.subplots(1, 1)
     plt_mean = ax.plot(pos, mu, color='#377eb8')
-    ax.fill_between(pos, mu - sigma, mu + sigma, alpha=0.3, color='#377eb8')
-    plt_median = ax.plot(pos, median, '-.', color='#e41a1c')
+    ax.fill_between(pos, np.maximum(mu - sigma, 0), mu + sigma, alpha=0.3,
+                    color='#377eb8')
+    # plt_median = ax.plot(pos, median, '-.', color='#e41a1c')
+    plt_cosine = ax.plot(pos, cosine(pos), '-.', color='#e41a1c')
     ax.set_xticks([])
     ax.set_yticks([])
     plt_aux = ax.fill(np.NaN, np.NaN, '#377eb8', alpha=0.3, linewidth=0)
-    ax.legend([(plt_mean[0], plt_aux[0]), plt_median[0]],
-              [r'Mean $\pm1$ STD', 'Median'],
+    ax.legend([(plt_mean[0], plt_aux[0]), plt_cosine[0]],
+              [r'Mean $\pm1$ STD', 'Truncated cosine'],
               loc='upper left', fontsize='xx-large')
     ax.set_title(r'Aligned $\mathbf{Y}$ columns', fontsize='xx-large')
     plt.savefig('{}{}Y_aligned_1d_summary.pdf'.format(dir_name, name), dpi=300)
