2cf149b5bfa4f7c6dbf5aa47f1010785e886bd2c
marianotepper <Lulita75>
2018-10-22 13:55:45 -0400

    Add colorbars and other minor plot modifications

--- experiments/test_clustering_toy.py
+++ experiments/test_clustering_toy.py
@@ -1,4 +1,4 @@
-from __future__ import absolute_import, print_function
+import matplotlib.gridspec as gridspec
 import matplotlib.pyplot as plt
 import os
 import seaborn as sns
@@ -14,7 +14,7 @@ if not os.path.exists(dir_name):
     os.mkdir(dir_name)
 
 
-def test_clustering(X, gt, n_clusters, filename):
+def test_clustering(X, gt, n_clusters, filename, palette='Set1'):
     Y = sdp_km_burer_monteiro(X, n_clusters, rank=len(X), tol=1e-6,
                               verbose=True)
     D = X.dot(X.T)
@@ -22,18 +22,21 @@ def test_clustering(X, gt, n_clusters, filename):
 
     sns.set_style('white')
     plt.figure(figsize=(12, 3.5), tight_layout=True)
+    gs = gridspec.GridSpec(1, 4, wspace=0.15)
 
-    ax = plt.subplot(141)
-    plot_data_clustered(X, gt, ax=ax)
+    ax = plt.subplot(gs[0])
+    plot_data_clustered(X, gt, ax=ax, palette=palette)
     ax.set_title('Input dataset', fontsize='xx-large')
 
     titles = [r'$\mathbf{X}^\top \mathbf{X}$',
               r'$\mathbf{Y}^\top \mathbf{Y}$',
               r'$\mathbf{Y}^\top$'
     ]
-    for i, (M, t) in enumerate(zip([D, Q, Y], titles)):
-        ax = plt.subplot(1, 4, i + 2)
-        plot_matrix(M, ax=ax)
+    for i, (M, t, wl) in enumerate(zip([D, Q, Y], titles,
+                                       ['both', 'both', 'vertical'])):
+        ax = plt.subplot(gs[i + 1])
+        plot_matrix(M, ax=ax, labels=gt, which_labels=wl,
+                    labels_palette=palette, colorbar_labelsize=15)
         ax.set_title(t, fontsize='xx-large')
 
     plt.tight_layout()
